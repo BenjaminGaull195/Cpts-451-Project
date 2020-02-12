8ff1a16a1e5458a855acdea61452c1c0e7b578c6
Ben Gaull <brgaull1998@hotmail.com>
2020-02-11 18:35:19 -0800

    Started sql

--- Milestone_1/buildDB.sql
+++ Milestone_1/buildDB.sql
@@ -1,7 +1,25 @@
 
 --Create User
 CREATE TABLE User(
-
+    userID INT NOT NULL,
+    uName VARCHAR(32),
+    uNum_Fans INT,
+    --Num Votes
+    uNum_Funny INT,
+    uNum_Cool INT,
+    uNum_Useful INT,
+    uTips_Count INT,
+    uTotal_Tip_Likes INT,
+    --Average Stars
+    uTotal_Stars INT,
+    uTotal_Ratings INT,
+    --Loacation
+    uLatitude FLOAT,
+    uLongitude FLOAT,
+    --Date Joined
+    uDate_Joined DATE,
+    uTime_Joined TIME,
+    CONSTRAINT pk_Constraint PRIMARY KEY(userID)
 );
 
 --Create Tips
