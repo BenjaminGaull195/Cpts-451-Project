cf78cc9d40bf4e6827ad5cf43a163c6689a86cd4
Ben Gaull <brgaull1998@hotmail.com>
2020-02-11 18:57:08 -0800

    added sql statements to create the User, Tips, Business tables

--- Milestone_1/buildDB.sql
+++ Milestone_1/buildDB.sql
@@ -19,17 +19,33 @@ CREATE TABLE User(
     --Date Joined
     uDate_Joined DATE,
     uTime_Joined TIME,
-    CONSTRAINT pk_Constraint PRIMARY KEY(userID)
+    CONSTRAINT pk_User PRIMARY KEY(userID)
 );
 
 --Create Tips
 CREATE TABLE Tips(
+    --Not certain for Primary key
 
+    tNum_Likes INT,
+    tPosted_Date DATE,
+    tPosted_Time TIME,
+    tText TEXT,
+    CONSTRAINT pk_Tips PRIMARY KEY()
 );
 
 --Create Business
 CREATE TABLE Business(
-
+    businessID INT NOT NULL,
+    bName VARCHAR(32),
+    --Address
+    bStreet VARCHAR(40),
+    bCity VARCHAR(20),
+    bState CHAR(2),
+    bPostal_Code INT,
+    --Location
+    bLatitude FLOAT,
+    bLongitude FLOAT,
+    CONSTRAINT pk_Business PRIMARY KEY(businessID)
 );
 
 --Create Friends Relationship
