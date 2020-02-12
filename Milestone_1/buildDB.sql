7b98febe8b48a43543b82cfbc14198ed457b3468
Ben Gaull <brgaull1998@hotmail.com>
2020-02-11 17:58:07 -0800

    Added sql file to build database tables

---
+++ Milestone_1/buildDB.sql
@@ -0,0 +1,42 @@
+
+--Create User
+CREATE TABLE User(
+
+);
+
+--Create Tips
+CREATE TABLE Tips(
+
+);
+
+--Create Business
+CREATE TABLE Business(
+
+);
+
+--Create Friends Relationship
+CREATE TABLE Friends(
+
+);
+
+--Create Makes_Tips Relationship
+CREATE TABLE Makes_Tips(
+
+);
+
+--Creates Tips_About Relationship
+CREATE TABLE Tips_About(
+
+);
+
+--Creates Checkin Relationship
+CREATE TABLE Checkin(
+
+);
+
+--Create Hours Weak Relation
+CREATE TABLE Hours_Open(
+
+);
+
+
